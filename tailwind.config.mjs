53a14c1711e7ca1f290c43ffb3f10b6c5756c7ce
Gerry Bogard <cruzgerry2520@gmail.com>
2024-04-22 16:13:07 -0600

    mitad del proyecto

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'cafecarrito': '#eba121', //Custom color for shopping cart icon
+				'#f2ca7f': '#f2ca7f',
+				'#fa991c': '#fa991c', //Custom colors for GET STARTED button
+				'#f9f7f7': '#f9f7f7', //Custom color for about box 2
+			},
+			backgroundImage: {
+				'main-pattern': "url('/image_1.webp')",
+			}
+		},
 	},
 	plugins: [],
 }
